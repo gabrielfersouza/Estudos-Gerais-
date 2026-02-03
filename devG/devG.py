d674717bac8aed471a8c323a751d50e8c704ba68
Gabriel Fernandes Souza <gabriel05fersouza@gmail.com>
2026-02-03 14:56:53 -0300

    Add files via upload

---
+++ devG/devG.py
@@ -0,0 +1,13 @@
+def mul(a, b):
+    
+    return a * b + a * (-b)/a+b
+
+
+a = int(input(f"digite o valor o pirmiero numero"))
+b = int(input(f"difgite o valor do segundo nuemro"))
+
+resul = mul(a,b)
+
+print(f"O valor do resultado e{resul}")
+
+
